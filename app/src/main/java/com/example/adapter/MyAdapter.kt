48b5dff5e8b4cad2d8b8ee3d0a5d654991340cb8
YehJiaJun <a159663@hotmail.com>
2021-01-26 18:29:21 +0800

    update

---
+++ app/src/main/java/com/example/adapter/MyAdapter.kt
@@ -0,0 +1,19 @@
+package com.example.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.adapter_vertical.view.*
+import java.util.ArrayList
+
+class MyAdapter(private val layout:Int, private val data: ArrayList<Item>):BaseAdapter() {
+    override fun getCount() = data.size
+    override fun getItem(position: Int) = data[position]
+    override fun getItemId(position: Int) = 0L
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view = View.inflate(parent?.context,layout,null)
+        view.img_photo.setImageResource(data[position].photo)
+        view.tv_name.text = data[position].name
+        return view
+    }
+}
